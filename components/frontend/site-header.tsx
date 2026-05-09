6e26ad08f3e4764dd1777e97f8719cd448c5a9ab
Ironpeptides <simiyunevily@gmail.com>
2026-05-09 10:52:15 +0300

    fix:Fixed the blogs route

--- components/frontend/site-header.tsx
+++ components/frontend/site-header.tsx
@@ -242,7 +242,7 @@ export default function SiteHeader({ session }: { session: Session | null }) {
               </NavigationMenuItem> */}
 
               <NavigationMenuItem>
-  <Link href="/blogs" legacyBehavior passHref>
+  <Link href="/blog" legacyBehavior passHref>
     <NavigationMenuLink className="h-9 px-3 py-2 text-sm font-bold text-gray-400 hover:text-blue-400 transition-colors uppercase tracking-tight">
       Blog
     </NavigationMenuLink>
